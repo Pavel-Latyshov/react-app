8846f0638ae260584bdc2d5a658cc1f7b8b287fb
Pavel Latyshov <platyshov@MacBook-Pro-Pavel.local>
2021-03-22 22:07:11 +0300

    feat: add profile and messages links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -7,11 +9,16 @@ import Profile from './components/Profile/Profile';
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
-    </div>
+      <div className="app-wrapper-content" >
+      <Route path="/dialogs" component={Dialogs} />
+      <Route path="/profile" component={Profile} />
+      </div>
+    </div>   
+    </BrowserRouter>
 
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import MyPosts from './MyPosts/MyPosts'
 import s from './Profile.module.css'
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://images.pexels.com/photos/2690323/pexels-photo-2690323.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"></img>
       </div>
